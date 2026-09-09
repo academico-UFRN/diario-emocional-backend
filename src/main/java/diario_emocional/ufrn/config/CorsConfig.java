4a40119e58b424363409bd6ac4bfc615da1cad2c
Pedro Varela <pedro.varela.700@ufrn.edu.br>
2026-09-09 20:11:19 -0300

    cors front vercel

--- src/main/java/diario_emocional/ufrn/config/CorsConfig.java
+++ src/main/java/diario_emocional/ufrn/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://diario-emocional-frontend-six.vercel.app/sentimentos")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
